ebf862f14d33a9c317004e40f279aa7761202fbc
Ömer Çıtak <msn@omercitak.net>
2015-02-07 12:19:52 +0200

    eksik harfler

--- src/tisikkirlir.php
+++ src/tisikkirlir.php
@@ -9,7 +9,7 @@ class tisikkirlir
 {	
 
 	public static function tsk($text){
-		$letter = array("a","e","ı","i","o","u","ü","A","E","I","İ","O","Ü");
+		$letter = array("a","e","ı","i","o","ö","u","ü","A","E","I","İ","O","Ö","U","Ü");
 		echo strtolower(str_replace($letter, "i", $text));
 	}
 
